ac66fccd25c81ea070112ec772572d16a80b30f0
Jeyasri Murugan <jeyasrim@syncfusion.com>
2023-11-30 11:04:21 +0530

    Code moved in MVVM

--- SchedulerMAUI/SchedulerMAUI/MainPage.xaml.cs
+++ SchedulerMAUI/SchedulerMAUI/MainPage.xaml.cs
@@ -5,161 +5,55 @@ namespace SchedulerMAUI
 {
     public partial class MainPage : ContentPage
     {
-        private SchedulerAppointment appointment;
-        private DateTime selectedDate;
-        public AppointmentEditorModel AppointmentEditorModel { get; set; }
         public MainPage()
         {
             InitializeComponent();
             this.Scheduler.DisplayDate = DateTime.Today.Date.AddHours(8);
-            this.AppointmentEditorModel = new AppointmentEditorModel();
-            this.sfPopup.BindingContext = this;
         }
 
         private void Scheduler_Tapped(object sender, SchedulerTappedEventArgs e)
         {
-            if (e.Appointments != null && e.Appointments.Count > 0)
+            if (this.BindingContext is SchedulerViewModel schedulerViewModel)
             {
-                appointment = (SchedulerAppointment)e.Appointments[0];
-                selectedDate = appointment.StartTime;
-            }
-            else
-            {
-                appointment = null;
-                selectedDate = (DateTime)e.Date;
-            }
-            this.UpdateEditor();
-            sfPopup.Show();
-        }
+                SchedulerAppointment appointment;
+                DateTime selectedDate;
 
-        private void DeleteButton_Clicked(object sender, EventArgs e)
-        {
-            if (appointment == null)
-            {
-                this.sfPopup.IsOpen = false;
-                return;
+                if (e.Appointments != null && e.Appointments.Count > 0)
+                {
+                    appointment = (SchedulerAppointment)e.Appointments[0];
+                    selectedDate = appointment.StartTime;
+                }
+                else
+                {
+                    appointment = null;
+                    selectedDate = (DateTime)e.Date;
+                }
+                schedulerViewModel.UpdateEditor(appointment, selectedDate);
+                sfPopup.Show();
             }
-
-            ((ObservableCollection<SchedulerAppointment>)this.Scheduler.AppointmentsSource).Remove(this.appointment);
-            var deleteAppointment = new Appointment() { From = appointment.StartTime, To = appointment.EndTime, AllDay = appointment.IsAllDay, Notes = appointment.Notes, EventName = appointment.Subject, ID = (int)appointment.Id };
-            App.Database.DeleteSchedulerAppointmentAsync(deleteAppointment);
-            this.sfPopup.IsOpen = false;
         }
 
         private void SwitchAllDay_Toggled(object sender, ToggledEventArgs e)
         {
-            if (AppointmentEditorModel.IsAllDay)
+            if (this.BindingContext is SchedulerViewModel schedulerViewModel)
             {
-                AppointmentEditorModel.StartTime = new TimeSpan(0, 0, 0);
-                AppointmentEditorModel.IsEditorEnabled = false;
-                AppointmentEditorModel.EndTime = new TimeSpan(0, 0, 0);
-            }
-            else
-            {
-                AppointmentEditorModel.IsEditorEnabled = true;
-            }
-        }
+                var appointmentEditorModel = schedulerViewModel.AppointmentEditorModel;
 
-        private void CancelButton_Clicked(object sender, EventArgs e)
-        {
-            this.sfPopup.IsOpen = false;
-        }
-        private void SaveButton_Clicked(object sender, EventArgs e)
-        {
-            var endDate = AppointmentEditorModel.EndDate;
-            var startDate = AppointmentEditorModel.StartDate;
-            var endTime = AppointmentEditorModel.EndTime;
-            var startTime = AppointmentEditorModel.StartTime;
-
-            if (endDate < startDate)
-            {
-                Application.Current.MainPage.DisplayAlert("", "End date should be greater than start date", "OK");
-            }
-            else if (endDate == startDate)
-            {
-                if (endTime <= startTime)
+                if (appointmentEditorModel == null)
                 {
-                    Application.Current.MainPage.DisplayAlert("", "End time should be greater than start time", "OK");
+                    return;
+                }
+
+                if (appointmentEditorModel.IsAllDay)
+                {
+                    appointmentEditorModel.StartTime = new TimeSpan(0, 0, 0);
+                    appointmentEditorModel.IsEditorEnabled = false;
+                    appointmentEditorModel.EndTime = new TimeSpan(0, 0, 0);
                 }
                 else
                 {
-                    AppointmentDetails();
+                    appointmentEditorModel.IsEditorEnabled = true;
                 }
-            }
-            else
-            {
-                AppointmentDetails();
-            }
-        }
-        private void AppointmentDetails()
-        {
-            if (appointment == null)
-            {
-                appointment = new SchedulerAppointment();
-                appointment.Subject = AppointmentEditorModel.Subject;
-                appointment.StartTime = AppointmentEditorModel.StartDate.Date.Add(AppointmentEditorModel.StartTime);
-                appointment.EndTime = AppointmentEditorModel.EndDate.Date.Add(AppointmentEditorModel.EndTime);
-                appointment.IsAllDay = AppointmentEditorModel.IsAllDay;
-                appointment.Notes = AppointmentEditorModel.Notes;
-
-                if (this.Scheduler.AppointmentsSource == null)
-                {
-                    this.Scheduler.AppointmentsSource = new ObservableCollection<SchedulerAppointment>();
-                }
-
-                appointment.Id = (this.Scheduler.AppointmentsSource as ObservableCollection<SchedulerAppointment>).Count;
-
-                ((ObservableCollection<SchedulerAppointment>)this.Scheduler.AppointmentsSource).Add(appointment);
-            }
-            else
-            {
-                appointment.Subject = AppointmentEditorModel.Subject;
-                appointment.StartTime = AppointmentEditorModel.StartDate.Date.Add(AppointmentEditorModel.StartTime);
-                appointment.EndTime = AppointmentEditorModel.EndDate.Date.Add(AppointmentEditorModel.EndTime);
-                appointment.IsAllDay = AppointmentEditorModel.IsAllDay;
-                appointment.Notes = AppointmentEditorModel.Notes;
-            }
-
-            //// - add or edit the appointment in the database collection
-            var editAppointment = new Appointment() { From = appointment.StartTime, To = appointment.EndTime, AllDay = appointment.IsAllDay, Notes = appointment.Notes, EventName = appointment.Subject, ID = (int)appointment.Id };
-            App.Database.SaveSchedulerAppointmentAsync(editAppointment);
-
-            this.sfPopup.IsOpen = false;
-        }
-        private void UpdateEditor()
-        {
-            if (appointment != null)
-            {
-                AppointmentEditorModel.Subject = appointment.Subject;
-                AppointmentEditorModel.Notes = appointment.Notes;
-                AppointmentEditorModel.StartDate = appointment.StartTime;
-                AppointmentEditorModel.EndDate = appointment.EndTime;
-                AppointmentEditorModel.IsEditorEnabled = true;
-
-                if (!appointment.IsAllDay)
-                {
-                    AppointmentEditorModel.StartTime = new TimeSpan(appointment.StartTime.Hour, appointment.StartTime.Minute, appointment.StartTime.Second);
-                    AppointmentEditorModel.EndTime = new TimeSpan(appointment.EndTime.Hour, appointment.EndTime.Minute, appointment.EndTime.Second);
-                    AppointmentEditorModel.IsAllDay = false;
-                    AppointmentEditorModel.IsEditorEnabled = true;
-                }
-                else
-                {
-                    AppointmentEditorModel.StartTime = new TimeSpan(12, 0, 0);
-                    AppointmentEditorModel.EndTime = new TimeSpan(12, 0, 0);
-                    AppointmentEditorModel.IsEditorEnabled = false;
-                    AppointmentEditorModel.IsAllDay = true;
-                }
-            }
-            else
-            {
-                AppointmentEditorModel.Subject = "";
-                AppointmentEditorModel.Notes = "";
-                AppointmentEditorModel.IsAllDay = false;
-                AppointmentEditorModel.StartDate = selectedDate;
-                AppointmentEditorModel.StartTime = new TimeSpan(selectedDate.Hour, selectedDate.Minute, selectedDate.Second);
-                AppointmentEditorModel.EndDate = selectedDate;
-                AppointmentEditorModel.EndTime = new TimeSpan(selectedDate.Hour + 1, selectedDate.Minute, selectedDate.Second);
             }
         }
     }
--- SchedulerMAUI/SchedulerMAUI/Viewmodel/SchedulerViewModel.cs
+++ SchedulerMAUI/SchedulerMAUI/Viewmodel/SchedulerViewModel.cs
@@ -1,12 +1,42 @@
 ﻿using Syncfusion.Maui.Scheduler;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace SchedulerMAUI
 {
-    public class SchedulerViewModel 
+    public class SchedulerViewModel : INotifyPropertyChanged
     {
+        private SchedulerAppointment appointment;
+        private DateTime selectedDate;
+
+        public Command AddAppointment { get; set; }
+
+        public Command DeleteAppointment { get; set; }
+
+        public Command CancelEditAppointment { get; set; }
+
+        public AppointmentEditorModel AppointmentEditorModel { get; set; }
+
+        public ObservableCollection<SchedulerAppointment> Appointments { get; set; }
+
+        private bool isOpen;
+        public bool IsOpen
+        {
+            get { return isOpen; }
+            set
+            {
+                isOpen = value;
+                OnPropertyChanged(nameof(this.IsOpen));
+            }
+        }
+
         public SchedulerViewModel()
         {
+            AddAppointment = new Command(AddAppointmentDetails);
+            DeleteAppointment = new Command(DeleteSchedulerAppointment);
+            CancelEditAppointment = new Command(CancelEdit);
+            this.AppointmentEditorModel = new AppointmentEditorModel();
+
             this.GenerateAppointments();
 
             var dataBaseAppointments = App.Database.GetSchedulerAppointment();
@@ -26,8 +56,6 @@ namespace SchedulerMAUI
             }
         }
 
-        public ObservableCollection<SchedulerAppointment> Appointments { get; set; }
-
         private void GenerateAppointments()
         {
             if (Appointments == null)
@@ -38,6 +66,143 @@ namespace SchedulerMAUI
             };
 
             }
+        }
+
+
+        private void DeleteSchedulerAppointment()
+        {
+            if (appointment == null)
+            {
+                this.IsOpen = false;
+                return;
+            }
+
+            //// Remove the appointments in the Scheduler.
+            Appointments.Remove(this.appointment);
+            //// Delete appointment in the database
+            var deleteAppointment = new Appointment() { From = appointment.StartTime, To = appointment.EndTime, AllDay = appointment.IsAllDay, Notes = appointment.Notes, EventName = appointment.Subject, ID = (int)appointment.Id };
+            App.Database.DeleteSchedulerAppointmentAsync(deleteAppointment);
+            this.IsOpen = false;
+        }
+
+        private void CancelEdit()
+        {
+            this.IsOpen = false;
+        }
+
+        private void AddAppointmentDetails()
+        {
+            var endDate = AppointmentEditorModel.EndDate;
+            var startDate = AppointmentEditorModel.StartDate;
+            var endTime = AppointmentEditorModel.EndTime;
+            var startTime = AppointmentEditorModel.StartTime;
+
+            if (endDate < startDate)
+            {
+                Application.Current.MainPage.DisplayAlert("", "End date should be greater than start date", "OK");
+            }
+            else if (endDate == startDate)
+            {
+                if (endTime <= startTime)
+                {
+                    Application.Current.MainPage.DisplayAlert("", "End time should be greater than start time", "OK");
+                }
+                else
+                {
+                    AppointmentDetails();
+                }
+            }
+            else
+            {
+                AppointmentDetails();
+            }
+        }
+        private void AppointmentDetails()
+        {
+            if (appointment == null)
+            {
+                appointment = new SchedulerAppointment();
+                appointment.Subject = AppointmentEditorModel.Subject;
+                appointment.StartTime = AppointmentEditorModel.StartDate.Date.Add(AppointmentEditorModel.StartTime);
+                appointment.EndTime = AppointmentEditorModel.EndDate.Date.Add(AppointmentEditorModel.EndTime);
+                appointment.IsAllDay = AppointmentEditorModel.IsAllDay;
+                appointment.Notes = AppointmentEditorModel.Notes;
+
+                if (this.Appointments == null)
+                {
+                    this.Appointments = new ObservableCollection<SchedulerAppointment>();
+                }
+
+                appointment.Id = Appointments.Count;
+                //// Add the appointments in the Scheduler.
+                Appointments.Add(appointment);
+            }
+            else
+            {
+                appointment.Subject = AppointmentEditorModel.Subject;
+                appointment.StartTime = AppointmentEditorModel.StartDate.Date.Add(AppointmentEditorModel.StartTime);
+                appointment.EndTime = AppointmentEditorModel.EndDate.Date.Add(AppointmentEditorModel.EndTime);
+                appointment.IsAllDay = AppointmentEditorModel.IsAllDay;
+                appointment.Notes = AppointmentEditorModel.Notes;
+            }
+
+            SaveSchedulerAppointmentAsync();
+
+            this.IsOpen = false;
+        }
+
+        private void SaveSchedulerAppointmentAsync()
+        {
+            //// - add or edit the appointment in the database collection
+            var editAppointment = new Appointment() { From = appointment.StartTime, To = appointment.EndTime, AllDay = appointment.IsAllDay, Notes = appointment.Notes, EventName = appointment.Subject, ID = (int)appointment.Id };
+            App.Database.SaveSchedulerAppointmentAsync(editAppointment);
+        }
+
+        internal void UpdateEditor(SchedulerAppointment appointment, DateTime selectedDate)
+        {
+            this.appointment = appointment;
+            this.selectedDate = selectedDate;   
+            if (this.appointment != null)
+            {
+                AppointmentEditorModel.Subject = this.appointment.Subject;
+                AppointmentEditorModel.Notes = this.appointment.Notes;
+                AppointmentEditorModel.StartDate = this.appointment.StartTime;
+                AppointmentEditorModel.EndDate = this.appointment.EndTime;
+                AppointmentEditorModel.IsEditorEnabled = true;
+
+                if (!this.appointment.IsAllDay)
+                {
+                    AppointmentEditorModel.StartTime = new TimeSpan(this.appointment.StartTime.Hour, this.appointment.StartTime.Minute, this.appointment.StartTime.Second);
+                    AppointmentEditorModel.EndTime = new TimeSpan(this.appointment.EndTime.Hour, this.appointment.EndTime.Minute, this.appointment.EndTime.Second);
+                    AppointmentEditorModel.IsAllDay = false;
+                    AppointmentEditorModel.IsEditorEnabled = true;
+                }
+                else
+                {
+                    AppointmentEditorModel.StartTime = new TimeSpan(12, 0, 0);
+                    AppointmentEditorModel.EndTime = new TimeSpan(12, 0, 0);
+                    AppointmentEditorModel.IsEditorEnabled = false;
+                    AppointmentEditorModel.IsAllDay = true;
+                }
+            }
+            else
+            {
+                AppointmentEditorModel.Subject = "";
+                AppointmentEditorModel.Notes = "";
+                AppointmentEditorModel.IsAllDay = false;
+                AppointmentEditorModel.StartDate = this.selectedDate;
+                AppointmentEditorModel.StartTime = new TimeSpan(this.selectedDate.Hour, this.selectedDate.Minute, this.selectedDate.Second);
+                AppointmentEditorModel.EndDate = this.selectedDate;
+                AppointmentEditorModel.EndTime = new TimeSpan(this.selectedDate.Hour + 1, this.selectedDate.Minute, this.selectedDate.Second);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
         }
     }
 }
